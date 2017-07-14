7b27b2688070d96b24b903f3faeb58f6ffbf0097
jparenas <juan.p.arenas1@gmail.com>
2017-07-14 15:39:41 -0500

    Finished the security UI!
    -The security UI is complete
    - Added functions to the server to request the location of an emergency and the information of a person
    - Added an authentication cache, hopefully it makes the requests go faster
    - Added some cleanup code to the server
    
    TODO:
    - Cleanup code
    - Documentation

---
+++ TigerTrek-iOS/TigerTrek/InformationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  InformationViewController.swift
+//  TigerTrek
+//
+//  Created by Juan Pablo Arenas on 14/7/17.
+//  Copyright © 2017 Juan Pablo Arenas. All rights reserved.
+//
+
+import UIKit
+
+class InformationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
